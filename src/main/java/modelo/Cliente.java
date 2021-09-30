97fce9f652db3a7e816c4add5d50d030a20f3b63
MIPC <MIPC@DESKTOP-SC56MTC>
2021-09-29 22:52:53 -0500

    Actualizacion

--- src/main/java/modelo/Cliente.java
+++ src/main/java/modelo/Cliente.java
@@ -1,5 +1,68 @@
 package modelo;
 
 public class Cliente {
+	
+	private long numeroCedula;
+	private String nombreCompleto;
+	private String direccion;
+	private long telefono;
+	private String correoElectronico;
+	
+	public Cliente() {
+		this.numeroCedula = 0;
+		this.nombreCompleto = "";
+		this.direccion = "";
+		this.telefono = 0;
+		this.correoElectronico = "";
+	}
+
+	public Cliente(long numeroCedula, String nombreCompleto, String direccion, long telefono,
+			String correoElectronico) {
+		this.numeroCedula = numeroCedula;
+		this.nombreCompleto = nombreCompleto;
+		this.direccion = direccion;
+		this.telefono = telefono;
+		this.correoElectronico = correoElectronico;
+	}
+
+	public long getNumeroCedula() {
+		return numeroCedula;
+	}
+
+	public void setNumeroCedula(long numeroCedula) {
+		this.numeroCedula = numeroCedula;
+	}
+
+	public String getNombreCompleto() {
+		return nombreCompleto;
+	}
+
+	public void setNombreCompleto(String nombreCompleto) {
+		this.nombreCompleto = nombreCompleto;
+	}
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+	public long getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(long telefono) {
+		this.telefono = telefono;
+	}
+
+	public String getCorreoElectronico() {
+		return correoElectronico;
+	}
+
+	public void setCorreoElectronico(String correoElectronico) {
+		this.correoElectronico = correoElectronico;
+	}	
 
 }
